607c8212658de7b14afd1627f8c50898b617cfd1
ahjohns <ahjohns@gmail.com>
2015-06-21 20:43:04 -0400

    update

--- experiencevector.java
+++ experiencevector.java
@@ -1,1 +1,12 @@
 
+String experience= System.getProperty("user.home") +
+        new String("/experience");
+        File file = new File(experience);
+
+        SentenceIterator iter = new FileSentenceIterator(new SentencePreProcessor() {
+@Override
+public String preProcess(String sentence) {
+        return new
+        InputHomogenization(sentence).transform();
+        }
+        },file);
